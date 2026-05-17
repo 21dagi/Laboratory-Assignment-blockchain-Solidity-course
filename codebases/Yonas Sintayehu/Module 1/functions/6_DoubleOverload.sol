aa7463b54ed8e787459066e627184429cef18ce4
Yonas <tsehayneh.sinishaw@gmail.com>
2026-05-17 22:30:49 +0300

    complete module 1

---
+++ codebases/Yonas Sintayehu/Module 1/functions/6_DoubleOverload.sol
@@ -0,0 +1,9 @@
+contract Contract {
+    function double(uint _value) external pure returns (uint) {
+        return _value * 2;
+    }
+
+    function double(uint _a, uint _b) external pure returns (uint, uint) {
+        return (_a * 2, _b * 2);
+    }
+}
