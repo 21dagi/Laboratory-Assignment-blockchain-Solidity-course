0d361f50d3a16c4be60a859e9324662a1ae0f944
Yonas <tsehayneh.sinishaw@gmail.com>
2026-05-17 22:31:45 +0300

    complete module 4

---
+++ codebases/Yonas Sintayehu/Module 4/voting/2_CastVote.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+
+    Proposal[] public proposals;
+
+    function newProposal(address target, bytes calldata data) external {
+        proposals.push(Proposal(target, data, 0, 0));
+    }
+
+    function castVote(uint proposalId, bool support) external {
+        if (support) {
+            proposals[proposalId].yesCount++;
+        } else {
+            proposals[proposalId].noCount++;
+        }
+    }
+}
