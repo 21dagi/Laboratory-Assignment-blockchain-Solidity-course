fcbdd4291a0a24b7dd297652b4d8839023e67d1c
Yonas <tsehayneh.sinishaw@gmail.com>
2026-05-17 22:31:04 +0300

    complete module 2

---
+++ codebases/Yonas Sintayehu/Module 2/escrow/4_Approval.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+
+    constructor(address _arbiter, address _beneficiary) payable {
+        depositor = msg.sender;
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+    }
+
+    function approve() external {
+        (bool s, ) = beneficiary.call{ value: address(this).balance }("");
+        require(s);
+    }
+}
